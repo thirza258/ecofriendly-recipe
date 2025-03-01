6cd20117060e4be49c81008bf3d36940201b5fb0
thirzq <thirzahmad@gmail.com>
2025-03-01 22:45:11 +0700

    add

--- ecofriendly-backend/recommender/views.py
+++ ecofriendly-backend/recommender/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import JsonResponse
-from django.views import View
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
 
 # Create your views here.
 class RecommenderView(View):
