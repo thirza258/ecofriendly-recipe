687bf146bcaf6e89008fd3c50577c250113344f4
thirzq <thirzahmad@gmail.com>
2025-03-02 22:26:55 +0700

    change url

--- ecofriendly_frontend/src/service.tsx
+++ ecofriendly_frontend/src/service.tsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8000/api/v1";
+const API_URL = "https://clownfish-app-lf647.ondigitalocean.app/api/v1";
 
 interface Response {
   status: string;
